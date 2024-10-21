8ab8dc7a7b692357b926b6957aa04b4f95f9703f
Nillerdata <nielsglensgaard@gmail.com>
2024-10-21 14:13:18 +0200

    lavede noget opgave

--- lek_18/src/opgave_3/Team.java
+++ lek_18/src/opgave_3/Team.java
@@ -1,7 +1,77 @@
 package opgave_3;
 
+import java.util.ArrayList;
+
 public class Team {
+    private String name;
+    private ArrayList<Player> players;
 
+    public String getName() {
+        return name;
+    }
 
+    public Team(String name) {
+        this.name = name;
+        players = new ArrayList<>();
 
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void printplayers() {
+        for (Player s : players) {
+            System.out.println("spiller navn : " + s.getName() + "\n spiller alder : " + s.getAge()
+                    + "\nscore er : " + s.getScore());
+        }
+    }
+
+    public double calcAverageAge() {
+        double averageAge = 0;
+        for (Player s : players) {
+            averageAge += s.getAge();
+        }
+        return averageAge / players.size();
+    }
+
+    public int calcTotalScore() {
+        int totaltScore = 0;
+        for (Player p : players) {
+            totaltScore += p.getScore();
+        }
+        return totaltScore;
+    }
+
+    int calcOldPlayersScore(int ageLimit) {
+        int Pensionister = 0;
+        for (Player p : players) {
+            if (p.getAge() > ageLimit) {
+                Pensionister += p.getScore();
+            }
+        }
+        return Pensionister;
+    }
+
+    public int maxScore() {
+        int max = Integer.MIN_VALUE;
+        for (Player p : players) {
+            if (p.getScore() > max) {
+                max = p.getScore();
+            }
+        }
+        return max;
+    }
+
+    public String bestplayer() {
+        int max = Integer.MIN_VALUE;
+        String bestPlayer = "";
+        for (Player p : players) {
+            if (p.getScore() > max) {
+                max = p.getScore();
+                bestPlayer = p.getName();
+            }
+        }
+        return bestPlayer;
+    }
 }
--- lek_18/src/opgave_3/teamApp.java
+++ lek_18/src/opgave_3/teamApp.java
@@ -1,7 +1,25 @@
 package opgave_3;
 
+import java.util.ArrayList;
+
 public class teamApp {
     public static void main(String[] args) {
+    Player p1 = new Player("John",24);
+    p1.addScore(3);
+    Player p2 = new Player("simon",24 );
+    p2.addScore(42);
+    Player p3 = new Player("Bo", 48);
+    p3.addScore(240);
+    Team t1 = new Team("vib");
+    t1.addPlayer(p1);
+    t1.addPlayer(p2);
+    t1.addPlayer(p3);
+        System.out.println("__________________");
+        System.out.println("average age is : " +t1.calcAverageAge());
+        System.out.println("calc total score is : " + t1.calcTotalScore());
+        System.out.println("score for players over 30 is : " + t1.calcOldPlayersScore(30));
+        System.out.println("max score is : " + t1.maxScore());
+
 
 
 
